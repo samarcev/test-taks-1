08ac9fb4a4095dd9c8ba4155c3b2faa87abafeb3
Максим Самарцев <web.samarcev@gmail.com>
2025-06-27 14:46:16 +0500

    interface for trading

---
+++ src/components/app/inventory-case.vue
@@ -0,0 +1,95 @@
+<script setup lang="ts">
+import InventoryItem from '@/components/app/inventory-item.vue'
+import type { InventoryItemInterface } from '@/shared/interfaces/inventory-item.ts'
+import { type PropType, ref } from 'vue'
+
+const { multi, items } = defineProps({
+  multi: {
+    type: Boolean,
+    default: false,
+  },
+  items: {
+    type: Array as PropType<InventoryItemInterface[]>,
+    default: () => [],
+  },
+  maxSelectItems: {
+    type: Number,
+    default: null,
+  },
+})
+const emit = defineEmits(['onSelect'])
+const selectedItems = ref<number[]>([])
+const onChange = (event: Event, item: number): void => {
+  const input = event.target as HTMLInputElement
+  handleSelect(item, input.checked)
+}
+
+const handleSelect = (item: number, isChecked: boolean) => {
+  if (multi) {
+    if (isChecked) {
+      selectedItems.value.push(item)
+    } else {
+      selectedItems.value = selectedItems.value.filter((el) => el !== item)
+    }
+  } else {
+    selectedItems.value = [item]
+  }
+
+  const result = multi
+    ? items.filter((i) => selectedItems.value.includes(i.id))
+    : items.find((i) => selectedItems.value.includes(i.id))
+
+  emit('onSelect', result)
+}
+</script>
+
+<template>
+  <div class="inventory">
+    <label v-for="item in items" :key="item.id" class="inventory-item">
+      <input
+        :type="multi ? 'checkbox' : 'radio'"
+        :checked="selectedItems.includes(item.id)"
+        :disabled="
+          maxSelectItems
+            ? selectedItems.length >= maxSelectItems && !selectedItems.includes(item.id)
+            : false
+        "
+        @change="(e) => onChange(e, item.id)"
+      />
+      <inventory-item :id="item.id" :name="item.name" />
+    </label>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.inventory {
+  display: grid;
+  grid-template-columns: repeat(5, 1fr);
+  gap: 20px;
+  &-item {
+    position: relative;
+    cursor: pointer;
+    input {
+      position: absolute;
+      top: 0;
+      right: 0;
+      left: 0;
+      bottom: 0;
+      z-index: -100;
+      opacity: 0;
+      visibility: hidden;
+      &:checked  {
+        & + div {
+          background-color: #d9d9d9;
+          border: 2px solid #000;
+        }
+      }
+    }
+    &:has(input:disabled) {
+      cursor: not-allowed;
+      filter: grayscale(0);
+      opacity: 0.5;
+    }
+  }
+}
+</style>
---
+++ src/components/app/inventory-item.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps({
+  id: {
+    type: Number,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="item">
+    {{ name }}
+  </div>
+</template>
+
+<style scoped lang="scss">
+.item {
+  width: 100px;
+  height: 100px;
+  background-color: #fff;
+  border-radius: 5px;
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-size: 16px;
+}
+</style>
